8848aa6cea13077b00cad1335c62e30b1aeb5bf7
chienhoangvanp <chienhoangvanp@gmail.com>
2022-09-05 21:57:19 +0700

    up

---
+++ src/main/java/com/project/jobworking/JobWorkingApplication.java
@@ -0,0 +1,13 @@
+package com.project.jobworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWorkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobWorkingApplication.class, args);
+    }
+
+}
